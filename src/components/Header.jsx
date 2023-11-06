95570ede68442922c9af5f34bcf9396444c47278
aydotjs <ayobami66@gmail.com>
2023-11-06 08:55:25 +0100

    settinng image attributes in react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import reactImage from "../assets/react-core-concepts.png"
+const randomReactDescriptions = ["Fundamental", "Dynamic", "Core", "Basic"];
+const getRandomInteger = (max) => Math.floor(Math.random() * (max + 1));
 const Header = () => {
+  const description =
+    randomReactDescriptions[
+      getRandomInteger(randomReactDescriptions.length - 1)
+    ];
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {`
+        ${description}
+         React concepts you will need for almost any app you are going to build!`}
+      </p>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
