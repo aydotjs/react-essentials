bacacda19a45d3a6eda71927b17246e2b54bd700
aydotjs <ayobami66@gmail.com>
2023-11-06 09:28:33 +0100

    using props to configure reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import CoreConcepts from "./components/CoreConcepts";
 import Header from "./components/Header";
-
+import componentImg from "./assets/components.png";
+import stateImg from "./assets/state-mgmt.png";
+import jsxImg from "./assets/jsx-ui.png";
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The Core UI building Block"
+              image={componentImg}
+            />
+            <CoreConcepts
+              title="State"
+              description="Enable Dynamic and interactive User Interfaces"
+              image={stateImg}
+            />
+            <CoreConcepts
+              title="Javascript XML"
+              description="Describes UI components with HTML-like syntax"
+              image={jsxImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The Core UI building Block"
+              image={componentImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
